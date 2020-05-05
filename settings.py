f7e49ded36d8ea882ce010dec00043ff61bb75fb
nineninenine <ehobart@gmail.com>
2020-05-04 22:58:51 -0500

    first commit

---
+++ settings.py
@@ -0,0 +1,11 @@
+
+
+class Settings():
+	"""class to store settings in space invaders"""
+
+	def __init__(self):
+		"""init settins in space invaders"""
+		#screen settings
+		self.screen_width = 1200
+		self.screen_height = 800
+		self.bg_color = (100, 100, 230)
