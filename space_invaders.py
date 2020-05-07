8da15068da47b0e6001ce7823032c9c9c40e0ee1
nineninenine <ehobart@gmail.com>
2020-05-06 23:12:58 -0500

    fixed minor typo

--- space_invaders.py
+++ space_invaders.py
@@ -17,19 +17,14 @@ def run_game():
 
 	#create a ship object. comes before while loop so
 	#we dont make a new ship object each time thru the loop.
-
-
 	ship = Ship(screen)
-
-
-	#test
 
 	# start the main loop for the game
 	while True:
 
 		#watch for keyboard and mouse events.
 		#this is an event loop
-		gf.check_events()
+		gf.check_events(ship)
 
 		#set the background color, repaint screen, draw ship on screen
 		gf.update_screen(ai_settings, screen, ship)
