279dcc16afbc78740178f11f00cbfb9c68af65dc
nineninenine <ehobart@gmail.com>
2020-05-10 15:28:54 -0500

    add alien

--- space_invaders.py
+++ space_invaders.py
@@ -4,6 +4,7 @@ from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
 import game_functions as gf
+from alien import Alien
 
 
 def run_game():
@@ -13,12 +14,16 @@ def run_game():
 	#create a settings object
 	ai_settings = Settings()
 
+	#create the game window
 	screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
 	pygame.display.set_caption("Space Invaders")
 
 	#create a ship object. comes before while loop so
 	#we dont make a new ship object each time thru the loop.
 	ship = Ship(ai_settings, screen)
+
+	#make an alien
+	alien = Alien(ai_settings, screen)
 
 	#make a group obj to store bullets in
 	bullets = Group()
@@ -37,7 +42,7 @@ def run_game():
 		gf.update_bullets(bullets)
 
 		#set the background color, repaint screen, draw ship on screen, bullets too
-		gf.update_screen(ai_settings, screen, ship, bullets)
+		gf.update_screen(ai_settings, screen, ship, alien, bullets)
 
 
 #init and start the main loop
