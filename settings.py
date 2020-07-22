61045d2386bdae321912b8e973aa22b2bc2c7ba2
nineninenine <ehobart@gmail.com>
2020-07-21 20:28:26 -0500

    wide bullets for testing

--- settings.py
+++ settings.py
@@ -15,7 +15,7 @@ class Settings():
 
 		#bullet settings
 		self.bullet_speed_factor = 1
-		self.bullet_width = 3
+		self.bullet_width = 300
 		self.bullet_height = 15
 		self.bullet_color = (60, 60, 60)
 		#limits the nubmer of bullets allowed onscreen to encourage accurate shooting
