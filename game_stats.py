79de3141166e84dd33112b6b65ef9482a4c6632f
nineninenine <ehobart@gmail.com>
2020-07-21 22:52:33 -0500

    first commit

---
+++ game_stats.py
@@ -0,0 +1,10 @@
+class GateStats():
+    """track stats for space invaders"""
+    def __init__(self, ai_settings):
+        """init stats"""
+        self.ai_settings = ai_settings
+        self.reset_stats()
+
+    def reset_stats(self):
+        """init stats that can change thru out the game"""
+        self.ships_left = self.ai_settings.ship_limit
