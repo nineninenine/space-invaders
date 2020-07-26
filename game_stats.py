e82c716254dd1ead3de26075e7ab31eb083e11c5
nineninenine <ehobart@gmail.com>
2020-07-26 16:35:24 -0500

    added flag to end game after all ships lost

--- game_stats.py
+++ game_stats.py
@@ -4,6 +4,7 @@ class GameStats():
         """init stats"""
         self.ai_settings = ai_settings
         self.reset_stats()
+        self.game_active = True
 
     def reset_stats(self):
         """init stats that can change thru out the game"""
