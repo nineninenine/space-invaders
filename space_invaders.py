968700e9ced64cdcc733a654ea6c279a907c87d2
nineninenine <ehobart@gmail.com>
2020-07-26 19:03:50 -0500

    add play button

--- space_invaders.py
+++ space_invaders.py
@@ -5,6 +5,7 @@ from settings import Settings
 from game_stats import GameStats
 from ship import Ship
 import game_functions as gf
+from button import Button
 
 
 
@@ -19,6 +20,8 @@ def run_game():
 	#create the game window
 	screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
 	pygame.display.set_caption("Space Invaders")
+	#make play button
+	play_button = Button(ai_settings, screen, "Play")
 	#create gamestats instance to store game stats
 	stats = GameStats(ai_settings)
 
@@ -53,8 +56,8 @@ def run_game():
 			#update the position of the aliens
 			gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
 
-			#set the background color, repaint screen, draw ship on screen, bullets too
-			gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+		#set the background color, repaint screen, draw ship on screen, bullets too
+		gf.update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button)
 
 
 #init and start the main loop
