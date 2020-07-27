d89464c396a361b8b9e9c37926bfdc464cf7358e
nineninenine <ehobart@gmail.com>
2020-07-26 22:40:47 -0500

    reorganize settings. add changing dfficulty sttngs

--- settings.py
+++ settings.py
@@ -4,19 +4,17 @@ class Settings():
 	"""class to store settings in space invaders"""
 
 	def __init__(self):
-		"""init settins in space invaders"""
+		"""init static settings in space invaders"""
 		#screen settings
 		self.screen_width = 1200
 		self.screen_height = 800
 		self.bg_color = (230, 230, 230)
 
 		#ship settings
-		self.ship_speed_factor = 1.5
 		#number of lives
 		self.ship_limit = 3
 
 		#bullet settings
-		self.bullet_speed_factor = 5
 		self.bullet_width = 300
 		self.bullet_height = 15
 		self.bullet_color = (60, 60, 60)
@@ -24,8 +22,32 @@ class Settings():
 		self.bullets_allowed = 3
 
 		#alien settings
-		self.alien_speed_factor = 1
 		#how fast the fleet drops
 		self.fleet_drop_speed = 100
+
+		#how quickly the game speeds up
+		self.speedup_scale = 1.1
+
+		self.initialize_dynamic_settings()
+
+
+	def initialize_dynamic_settings(self):
+		"""init settings that change during the game"""
+		#ship settings
+		self.ship_speed_factor = 1.5
+		#bullet settings
+		self.bullet_speed_factor = 4
+
+		#alien settings
+		self.alien_speed_factor = 1
+
 		#fleet direction of 1 == right, -1 == left
 		self.fleet_direction = 1
+
+
+	def increase_speed(self):
+	"""increase speed settings"""
+	self.ship_speed_factor *= self.speedup_scale
+	self.bullet_speed_factor *= self.speedup_scale
+	self.alien_speed_factor *= self.speedup_scale
+
